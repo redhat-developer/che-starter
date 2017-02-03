f961ee2b32f12d8eab7bb44822b3cd4c4ee90514
Ilya Buziuk <ibuziuk@redhat.com>
2017-02-03 08:41:57 +0100

    Adding integration test for CheRestClient
    Signed-off-by: Ilya Buziuk <ibuziuk@redhat.com>

---
+++ src/test/java/io/fabric8/planner/che/starter/client/CheRestClientTest.java
@@ -0,0 +1,54 @@
+/*-
+ * #%L
+ * che-starter
+ * %%
+ * Copyright (C) 2017 Red Hat, Inc.
+ * %%
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * #L%
+ */
+package io.fabric8.planner.che.starter.client;
+
+import static org.testng.Assert.assertFalse;
+import static org.testng.Assert.assertTrue;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.logging.Log;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import io.fabric8.planner.che.starter.model.Workspace;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+public class CheRestClientTest {
+
+    private static final Logger LOG = LogManager.getLogger(CheRestClientTest.class);
+
+    @Autowired
+    private CheRestClient client;
+
+    @Test
+    public void listWorkspaceTest() {
+        List<Workspace> workspaces = this.client.listWorkspaces();
+        LOG.info("Number of workspaces: {}", workspaces.size());
+        workspaces.forEach(w -> LOG.info("workspace ID: {}", w.getId()));
+        List<Workspace> runningWorkspaces = workspaces.stream().filter(w -> w.getStatus().equals("RUNNING"))
+                .collect(Collectors.toList());
+        assertFalse(workspaces.isEmpty());
+        assertTrue(CollectionUtils.isEqualCollection(workspaces, runningWorkspaces));
+    }
+
+}
