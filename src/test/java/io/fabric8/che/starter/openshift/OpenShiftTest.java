cf8f897447a0ca8a615d73cea30dd47604bd7acb
Ilya Buziuk <ibuziuk@redhat.com>
2017-02-13 20:15:34 +0100

    Adding junit @Ignore import
    Signed-off-by: Ilya Buziuk <ibuziuk@redhat.com>

--- src/test/java/io/fabric8/che/starter/openshift/OpenShiftTest.java
+++ src/test/java/io/fabric8/che/starter/openshift/OpenShiftTest.java
@@ -17,6 +17,7 @@ import java.util.List;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
