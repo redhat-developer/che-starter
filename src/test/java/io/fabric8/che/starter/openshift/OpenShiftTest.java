03f52decbdd912d475c071b17c5baa9ae95fab71
Ilya Buziuk <ibuziuk@redhat.com>
2017-02-13 20:07:02 +0100

    OpenShift test refactoring
    Signed-off-by: Ilya Buziuk <ibuziuk@redhat.com>

--- src/test/java/io/fabric8/che/starter/openshift/OpenShiftTest.java
+++ src/test/java/io/fabric8/che/starter/openshift/OpenShiftTest.java
@@ -17,16 +17,15 @@ import java.util.List;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
 
 import io.fabric8.che.starter.TestConfig;
-import io.fabric8.kubernetes.api.model.HasMetadata;
 import io.fabric8.kubernetes.client.Config;
 import io.fabric8.kubernetes.client.ConfigBuilder;
 import io.fabric8.openshift.api.model.Parameter;
+import io.fabric8.openshift.api.model.ProjectRequest;
 import io.fabric8.openshift.api.model.Template;
 import io.fabric8.openshift.client.DefaultOpenShiftClient;
 import io.fabric8.openshift.client.OpenShiftClient;
@@ -39,7 +38,7 @@ public class OpenShiftTest extends TestConfig {
 
     @Value("${che.openshift.endpoint}")
     private String endpoint;
-    
+
     @Value("${che.openshift.project}")
     private String project;
 
@@ -52,38 +51,76 @@ public class OpenShiftTest extends TestConfig {
     @Ignore("Test is run against local minishift cluster and requires additional setup")
     @Test
     public void createCheServer() throws IOException {
-        Config config = new ConfigBuilder().
-                withMasterUrl(endpoint).
-                withUsername(username).
-                withPassword(password).build();
+        Config config = new ConfigBuilder()
+                            .withMasterUrl(endpoint)
+                            .withUsername(username)
+                            .withPassword(password)
+                            .build();
 
         OpenShiftClient client = new DefaultOpenShiftClient(config);
-        
-        LOG.info("Number of projects: {}", client.projects().list().getItems().size());
-        LOG.info("Projects: {}", client.projects().list());
 
-        Template template = client.templates().load(cheServerTemplate.getInputStream()).get();
+        LOG.info("Number of projects: {}", getNumberOfProjects(client));
+        ProjectRequest projectRequest = createTestProject(client);
+        LOG.info("Number of projects: {}", getNumberOfProjects(client));
+        
+        LOG.info("Test project has been deleted: {}", deleteTestProject(client, projectRequest));
+        LOG.info("Number of projects: {}", getNumberOfProjects(client));
+
+        Template template = loadTemplate(client);
+        LOG.info("Number of templates", getNumberOfTemplates(client));
 
         List<Parameter> parameters = template.getParameters();
         LOG.info("Number of template parameters: {}", parameters.size());
         for (Parameter parameter : parameters) {
-            LOG.info("Template Parameter: {}", parameter);
+            LOG.info("Template Parameter: {}", parameter.getName());
         }
 
-        template.setParameters(parameters);
+        LOG.info("Number of templates", getNumberOfTemplates(client));
+        installTemplate(client, template);
+        LOG.info("Number of templates", getNumberOfTemplates(client));
 
-        LOG.info("Number of templates", client.templates().inNamespace(project).list().getItems().size());
+        LOG.info("Template has been deleted: {}", deleteTemplate(client, template));
+        LOG.info("Number of templates", getNumberOfTemplates(client));
 
-        client.templates().inNamespace(project).createOrReplace(template);
+        LOG.info("Pods: {}", getNumberOfPods(client));
+    }
 
-        LOG.info("Number of templates", client.templates().inNamespace(project).list().getItems().size());
+    private int getNumberOfProjects(OpenShiftClient client) {
+        return client.projects().list().getItems().size();
+    }
 
-        Boolean isDeleted = client.resource(template).inNamespace(project).delete();
-        LOG.info("Templates has been deleted: {}", isDeleted);
-        
-        LOG.info("Number of templates", client.templates().inNamespace(project).list().getItems().size());
+    private int getNumberOfTemplates(OpenShiftClient client) {
+        return client.templates().inNamespace(project).list().getItems().size();
+    }
 
-        LOG.info("Pods: {}", client.pods().inNamespace(project).list());
+    private int getNumberOfPods(OpenShiftClient client) {
+        return client.pods().inNamespace(project).list().getItems().size();
+    }
+
+    private ProjectRequest createTestProject(OpenShiftClient client) {
+        return client.projectrequests().
+               createNew().
+               withNewMetadata().
+               withName("test-project").
+               endMetadata().
+               withDescription("Test Project").
+               withDisplayName("Test Project").done();
+    }
+
+    private boolean deleteTestProject(OpenShiftClient client, ProjectRequest projectRequest) {
+        return client.projects().withName(projectRequest.getMetadata().getName()).delete();
+    }
+
+    private Template loadTemplate(OpenShiftClient client) throws IOException {
+        return client.templates().load(cheServerTemplate.getInputStream()).get();
+    }
+
+    private Template installTemplate(OpenShiftClient client, Template template) throws IOException {
+        return client.templates().inNamespace(project).createOrReplace(template);
+    }
+
+    private boolean deleteTemplate(OpenShiftClient client, Template template) {
+        return client.resource(template).inNamespace(project).delete();
     }
 
 }
