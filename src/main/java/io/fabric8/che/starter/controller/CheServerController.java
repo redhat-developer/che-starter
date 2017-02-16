4bfd89137ebbbdadb2619a1ec1cac0c843489e1a
Ilya Buziuk <ibuziuk@redhat.com>
2017-02-16 13:03:16 +0100

    Initial CheServerController implementation
    Signed-off-by: Ilya Buziuk <ibuziuk@redhat.com>

--- src/main/java/io/fabric8/che/starter/controller/CheServerController.java
+++ src/main/java/io/fabric8/che/starter/controller/CheServerController.java
@@ -13,6 +13,8 @@
 package io.fabric8.che.starter.controller;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -27,17 +29,27 @@ import org.springframework.web.bind.annotation.RestController;
 import io.fabric8.che.starter.model.OpenShiftConfig;
 import io.fabric8.che.starter.model.request.CheServerCreateParams;
 import io.fabric8.che.starter.model.response.CheServerInfo;
+import io.fabric8.kubernetes.api.model.KubernetesList;
 import io.fabric8.kubernetes.client.Config;
 import io.fabric8.kubernetes.client.ConfigBuilder;
+import io.fabric8.openshift.api.model.DoneableTemplate;
+import io.fabric8.openshift.api.model.Parameter;
+import io.fabric8.openshift.api.model.ProjectRequest;
 import io.fabric8.openshift.api.model.Template;
 import io.fabric8.openshift.client.DefaultOpenShiftClient;
 import io.fabric8.openshift.client.OpenShiftClient;
+import io.fabric8.openshift.client.ParameterValue;
+import io.fabric8.openshift.client.dsl.ClientTemplateResource;
 import io.swagger.annotations.ApiOperation;
 
 @CrossOrigin
 @RestController
 @RequestMapping("/server")
 public class CheServerController {
+    private static final String CHE_PROJECT_NAME = "che-server-project";
+    private static final String CHE_PROJECT_DISPLAY_NAME = "Che Server";
+    private static final String CHE_PROJECT_DESCRIPTION = "Che Server";
+    private static final String CHE_OPENSHIFT_ENDPOINT = "CHE_OPENSHIFT_ENDPOINT";
 
     private static final Logger LOG = LogManager.getLogger(CheServerController.class);
 
@@ -52,10 +64,61 @@ public class CheServerController {
         String masterUrl = config.getMasterURL();
         LOG.info("OpenShift master URL: {}", masterUrl);
 
-        Config openshiftConfig = new ConfigBuilder().withMasterUrl(masterUrl).withOauthToken(userToken).build();
+        Config openshiftConfig = getConfig(masterUrl, userToken);
         OpenShiftClient client = new DefaultOpenShiftClient(openshiftConfig);
-        Template template = client.templates().load(cheServerTemplate.getInputStream()).get();
+
+        createProject(client);
+        List<ParameterValue> parameterValues = getTemplateParameterValues(client, masterUrl);
+        KubernetesList kubernetesList = processTemplate(client, parameterValues);
+        createResources(client, kubernetesList);
+
         return new CheServerInfo();
+    }
+
+    private ProjectRequest createProject(OpenShiftClient client) {
+        return client.projectrequests().
+                      createNew().
+                      withNewMetadata().
+                      withName(CHE_PROJECT_NAME).
+                      endMetadata().
+                      withDescription(CHE_PROJECT_DESCRIPTION).
+                      withDisplayName(CHE_PROJECT_DISPLAY_NAME).
+                      done();
+    }
+
+    private Config getConfig(String masterURL, String token) {
+        return new ConfigBuilder().withMasterUrl(masterURL).withOauthToken(token).build();
+    }
+
+    private List<ParameterValue> getTemplateParameterValues(OpenShiftClient client, String masterURL)
+            throws IOException {
+        Template template = loadTemplate(client);
+        List<ParameterValue> pvs = new ArrayList<>();
+        for (Parameter parameter : template.getParameters()) {
+            String name = parameter.getName();
+            String value = parameter.getValue();
+            LOG.info("Template Parameter Name: {}", name);
+            LOG.info("Template Parameter Value: {}", value);
+            if (CHE_OPENSHIFT_ENDPOINT.equals(name) && value.isEmpty()) {
+                value = masterURL;
+            }
+        }
+        return pvs;
+    }
+
+    private Template loadTemplate(OpenShiftClient client) throws IOException {
+        return client.templates().load(cheServerTemplate.getInputStream()).get();
+    }
+
+    private KubernetesList processTemplate(OpenShiftClient client, List<ParameterValue> parameterValues)
+            throws IOException {
+        ClientTemplateResource<Template, KubernetesList, DoneableTemplate> templateHandle = client.templates()
+                .load(cheServerTemplate.getInputStream());
+        return templateHandle.process(parameterValues.toArray(new ParameterValue[parameterValues.size()]));
+    }
+
+    private KubernetesList createResources(OpenShiftClient client, KubernetesList list) {
+        return client.lists().inNamespace(CHE_PROJECT_NAME).create(list);
     }
 
 }
