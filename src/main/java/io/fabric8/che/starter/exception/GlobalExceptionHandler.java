ab2536e68b00b25040f31343aab425c507f60cf6
Ilya Buziuk <ibuziuk@redhat.com>
2017-02-24 15:36:32 +0100

    Adding more ExceptionHandlers
    Signed-off-by: Ilya Buziuk <ibuziuk@redhat.com>

--- src/main/java/io/fabric8/che/starter/exception/GlobalExceptionHandler.java
+++ src/main/java/io/fabric8/che/starter/exception/GlobalExceptionHandler.java
@@ -14,11 +14,14 @@ package io.fabric8.che.starter.exception;
 
 import java.net.MalformedURLException;
 import java.net.URISyntaxException;
+import java.net.UnknownHostException;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import io.fabric8.kubernetes.client.KubernetesClientException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -26,6 +29,18 @@ public class GlobalExceptionHandler {
     @ResponseStatus(value = HttpStatus.BAD_REQUEST, reason = "URL is not valid")
     @ExceptionHandler({ URISyntaxException.class, MalformedURLException.class })
     public String handleURLException(Exception e) {
+        return e.getMessage();
+    }
+
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST, reason = "MasterUrl is not valid")
+    @ExceptionHandler(UnknownHostException.class)
+    public String handleHostException(UnknownHostException e) {
+        return e.getMessage();
+    }
+
+    @ResponseStatus(value = HttpStatus.FORBIDDEN)
+    @ExceptionHandler(KubernetesClientException.class)
+    public String handleKubernetesClientException(KubernetesClientException e) {
         return e.getMessage();
     }
 
