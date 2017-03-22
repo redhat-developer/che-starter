bfadaaa8decd9b90ca5cad3d8dfe67ba7444c523
Ilya Buziuk <ibuziuk@redhat.com>
2017-03-22 19:49:10 +0100

    Extracting Project client
    Signed-off-by: Ilya Buziuk <ibuziuk@redhat.com>

--- src/main/java/io/fabric8/che/starter/model/request/TokenCreateParams.java
+++ src/main/java/io/fabric8/che/starter/model/request/TokenCreateParams.java
@@ -21,4 +21,12 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class TokenCreateParams {
     private String token;
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
 }
