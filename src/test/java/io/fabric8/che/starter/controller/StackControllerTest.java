6664823fcee011bef1d7734cdfd7c3f63f22a18d
Marian Labuda <mlabuda@redhat.com>
2017-04-04 15:25:13 +0200

    Workspace Controller tests for endpoints added

--- src/test/java/io/fabric8/che/starter/controller/StackControllerTest.java
+++ src/test/java/io/fabric8/che/starter/controller/StackControllerTest.java
@@ -17,42 +17,46 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import org.junit.Before;
+import java.io.IOException;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 
 import io.fabric8.che.starter.TestBase;
 
 @RunWith(SpringRunner.class)
-@WebAppConfiguration
+@AutoConfigureMockMvc
 @SpringBootTest(properties = { "OPENSHIFT_TOKEN_URL=http://localhost:33333/keycloak/token/openshift",
 		"GITHUB_TOKEN_URL=http://localhost:33333/keycloak/token/github" })
 public class StackControllerTest extends TestBase {
-
+	
 	private static final String STACK_OSO_ENDPOINT = "/stack/oso";
 	private static final String STACK_ENDPOINT = "/stack";
-
+	
 	@Autowired
-	private WebApplicationContext wac;
-
 	private MockMvc mockMvc;
 
-	@Before
-	public void setup() {
-		this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
+	@BeforeClass
+	public static void setUp() throws IOException {
+		VertxServer.getInstance().startVertxServer();
 	}
-
+	
+	@AfterClass
+	public static void destroy() throws Exception {
+		VertxServer.getInstance().stopVertxServer();
+	}
+	
 	@Test
 	public void testGetStacksOSO() throws Exception {
 		mockMvc.perform(get(STACK_OSO_ENDPOINT).header("Authorization", OPENSHIFT_TOKEN)
-				.param("masterUrl", VERTX_SERVER).param("namespace", NAMESPACE)).andExpect(status().isOk())
+				.param("masterUrl", VERTX_SERVER).param("namespace", NAMESPACE)).andExpect(status().is2xxSuccessful())
 				.andExpect(jsonPath("$[1].id", is("java-default"))).andExpect(jsonPath("$[0].id", is("vert.x")));
 	}
 
@@ -77,7 +81,7 @@ public class StackControllerTest extends TestBase {
 	@Test
 	public void testGetStacks() throws Exception {
 		mockMvc.perform(get(STACK_ENDPOINT).header("Authorization", KEYCLOAK_TOKEN).param("masterUrl", VERTX_SERVER)
-				.param("namespace", NAMESPACE)).andExpect(status().isOk())
+				.param("namespace", NAMESPACE)).andExpect(status().is2xxSuccessful())
 				.andExpect(jsonPath("$[1].id", is("java-default"))).andExpect(jsonPath("$[0].id", is("vert.x")));
 	}
 
@@ -97,5 +101,5 @@ public class StackControllerTest extends TestBase {
 	public void testGetStacksWithWrongMasterURL() throws Exception {
 		mockMvc.perform(get(STACK_ENDPOINT).header("Authorization", KEYCLOAK_TOKEN)
 				.param("masterUrl", "http://i.do.not.exist").param("namespace", NAMESPACE)).andExpect(status().is(401));
-	}
+	} 
 }
