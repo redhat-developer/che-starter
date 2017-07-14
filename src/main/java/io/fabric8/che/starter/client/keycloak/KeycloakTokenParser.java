fec818b4257b77143bd793af788cb23123a0c563
Ilya Buziuk <ibuziuk@redhat.com>
2017-07-14 11:00:39 +0200

    che-starter #196: Add token.payload['sub'] as identity_id when logging where relevant
    Signed-off-by: Ilya Buziuk <ibuziuk@redhat.com>

---
+++ src/main/java/io/fabric8/che/starter/client/keycloak/KeycloakTokenParser.java
@@ -0,0 +1,48 @@
+/*-
+ * #%L
+ * che-starter
+ * %%
+ * Copyright (C) 2017 Red Hat, Inc.
+ * %%
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * #L%
+ */
+package io.fabric8.che.starter.client.keycloak;
+
+import java.io.IOException;
+
+import org.springframework.stereotype.Component;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Header;
+import io.jsonwebtoken.Jwt;
+import io.jsonwebtoken.Jwts;
+
+@Component
+public class KeycloakTokenParser {
+    private static final String TOKEN_PREFIX = "Bearer ";
+
+    public String getIdentityId(final String keycloakToken) throws JsonProcessingException, IOException {
+        String jwt = keycloakToken.replaceFirst(TOKEN_PREFIX, "");
+        String tokenWithoutSignature = getJWSWithoutSignature(jwt);
+        Jwt<Header, Claims> untrusted = Jwts.parser().parseClaimsJwt(tokenWithoutSignature);
+        return untrusted.getBody().getSubject();
+    }
+
+    /**
+     * @see <a href=
+     *      "https://github.com/jwtk/jjwt/issues/67#issuecomment-182527735">Ability
+     *      to inspect body of signed JWT</a>
+     */
+    private String getJWSWithoutSignature(final String token) {
+        int i = token.lastIndexOf('.');
+        String withoutSignature = token.substring(0, i + 1);
+        return withoutSignature;
+    }
+
+}
--- src/main/java/io/fabric8/che/starter/listener/RequestListener.java
+++ src/main/java/io/fabric8/che/starter/listener/RequestListener.java
@@ -20,24 +20,51 @@ import javax.servlet.http.HttpServletRequest;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.MDC;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import io.fabric8.che.starter.client.keycloak.KeycloakTokenParser;
 
 @WebListener
 public class RequestListener implements ServletRequestListener {
     private static final String REQUEST_ID_HEADER = "X-Request-Id";
+    private static final String AUTHORIZATION_HEADER = "Authorization";
     private static final String REQUEST_ID_MDC_KEY = "req_id";
+    private static final String IDENTITY_ID_MDC_KEY = "identity_id";
+    private static final String UNKNOWN_IDENTITY_ID = "Unknown";
 
-    
+    @Autowired
+    KeycloakTokenParser keycloakTokenParser;
+
     @Override
     public void requestInitialized(ServletRequestEvent event) {
         HttpServletRequest request = (HttpServletRequest) event.getServletRequest();
         String requestId = request.getHeader(REQUEST_ID_HEADER);
         requestId = (StringUtils.isBlank(requestId)) ? generateRequestId() : requestId;
+
+        String keycloakToken = request.getHeader(AUTHORIZATION_HEADER);
+        String identityId = getIdentityId(keycloakToken);
+
         MDC.put(REQUEST_ID_MDC_KEY, requestId);
+        MDC.put(IDENTITY_ID_MDC_KEY, identityId);
     }
 
     @Override
     public void requestDestroyed(ServletRequestEvent event) {
         MDC.clear();
+    }
+
+    private String getIdentityId(final String keycloakToken) {
+        String identityId;
+        if (StringUtils.isBlank(keycloakToken)) {
+            identityId = UNKNOWN_IDENTITY_ID;
+        } else {
+            try {
+                identityId = keycloakTokenParser.getIdentityId(keycloakToken);
+            } catch (Exception e) {
+                identityId = UNKNOWN_IDENTITY_ID;
+            }
+        }
+        return identityId;
     }
 
     private String generateRequestId() {
