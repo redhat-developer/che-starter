961af9753d57ee694d36f62d0a74d796d1eec771
Ilya Buziuk <ibuziuk@redhat.com>
2018-02-22 16:18:54 +0100

    Running che-starter integration tests against prod-preview using keycloak token from 'che_functional_tests_credentials_wrapper'
    Signed-off-by: Ilya Buziuk <ibuziuk@redhat.com>

--- cico_run_tests.sh
+++ cico_run_tests.sh
@@ -12,11 +12,24 @@
 # http://www.eclipse.org/legal/epl-v10.html
 # #L%
 ###
+cat jenkins-env | grep -e KEYCLOAK_TOKEN > inherit-env
 . inherit-env
 
 yum -y update
-yum -y install centos-release-scl java-1.8.0-openjdk-devel
+yum -y install centos-release-scl java-1.8.0-openjdk-devel curl
 yum -y install rh-maven33
+
+# installing jq via curl since 'No package jq available' for yum
+curl -LO https://github.com/stedolan/jq/releases/download/jq-1.5/jq-linux64
+mv jq-linux64 /usr/bin/jq
+chmod +x /usr/bin/jq
+
+# Keycloak token provided by `che_functional_tests_credentials_wrapper` from `openshiftio-cico-jobs` is a refresh token. 
+# Obtaining osio user token
+AUTH_RESPONSE=$(curl -H "Content-Type: application/json" -X POST -d '{"refresh_token":"'$KEYCLOAK_TOKEN'"}' https://auth.prod-preview.openshift.io/api/token/refresh)
+
+# `OSIO_USER_TOKEN` is used for che-starter integration tests which are run against prod-preview
+export OSIO_USER_TOKEN=$(echo $AUTH_RESPONSE | jq --raw-output ".token | .access_token")
 
 scl enable rh-maven33 'mvn clean verify -B'
 
