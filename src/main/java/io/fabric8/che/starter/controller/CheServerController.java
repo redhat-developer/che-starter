7d8c4fc6cb938aa1ccd66bd1eb4d8c385d5fa208
Ilya Buziuk <ibuziuk@redhat.com>
2018-03-08 12:37:33 +0100

    rh-che-583: Adding support of unleash che 6 toggle
    Signed-off-by: Ilya Buziuk <ibuziuk@redhat.com>

--- src/main/java/io/fabric8/che/starter/controller/CheServerController.java
+++ src/main/java/io/fabric8/che/starter/controller/CheServerController.java
@@ -15,6 +15,9 @@ package io.fabric8.che.starter.controller;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -22,8 +25,10 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.fabric8.che.starter.client.keycloak.KeycloakTokenParser;
 import io.fabric8.che.starter.client.keycloak.KeycloakTokenValidator;
 import io.fabric8.che.starter.model.server.CheServerInfo;
+import io.fabric8.che.starter.toggle.Che6Toggle;
 import io.fabric8.che.starter.util.CheServerHelper;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
@@ -31,12 +36,25 @@ import io.swagger.annotations.ApiParam;
 @CrossOrigin
 @RestController
 public class CheServerController {
+    private static final Logger LOG = LoggerFactory.getLogger(CheServerController.class);
+
+    @Autowired
+    Che6Toggle che6toggle;
+
+    @Autowired
+    KeycloakTokenParser keycloakTokenParser;
 
     @ApiOperation(value = "Get Che server info")
     @GetMapping("/server")
     public CheServerInfo getCheServerInfo(@RequestParam String masterUrl, @RequestParam String namespace,
             @ApiParam(value = "Keycloak token", required = true) @RequestHeader("Authorization") String keycloakToken, HttpServletRequest request) throws Exception {
         KeycloakTokenValidator.validate(keycloakToken);
+
+        if (che6toggle.isChe6(keycloakToken)) {
+            String identityId = keycloakTokenParser.getIdentityId(keycloakToken);
+            LOG.info("User '{}' should be migrated to che 6", identityId);
+        }
+
         return getCheServerInfo(request);
       
     }
